a6571888366d75693b81ee8e496161d177be7251
hamupapa <k.mine2012@gmail.com>
2021-12-26 00:30:43 +0900

    htmlファイル追加

--- flaskKOH/run.py
+++ flaskKOH/run.py
@@ -1,11 +1,12 @@
 from os import name
 from flask import Flask
+from flask import request, redirect, url_for, render_template, flash, session
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-        name = "Hello World"
-        return name
+def show_index():
+    values = {"val1":100, "val2":200}
+    return render_template('index.html', values = values)
 
 @app.route('/good')
 def good():
